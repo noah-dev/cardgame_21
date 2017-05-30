d1f4e427cfbf81db7ed365c93829287d19eda447
local <local@local>
2017-05-29 23:43:44 -0500

    Interesting issue with BlackJackHand.hand - both dealer and player were pointing to same thing, effecively sharing a hand of cards. Test code remains

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,27 @@
 from random import shuffle
 
+# How classes are organized
+# Game
+# |--dealer
+# |  |--hand (hand class)
+# |  |--deck
+# |     |--card
+# |--player hand 1 (hand class)
+# |--player hand 2 (hand class)
+# |--etc...
+
+# game is responsible for managing the transfer of cards between the deck
+# and player hands.
+
+# dealer is responsible for managing it's own hand and the deck
+
+# hand class is responsible for managing a hand of cards for it's owner
+# (owner being the dealer or a player)
+
+# deck is a digital representation of a standard 52 card deck
+
+# card is a digital representation of a single card
+
 
 class Card:
     rank = ""
@@ -26,6 +48,8 @@ class Deck:
         # Build unshuffled_deck deck
         self.unshuffled_deck()
         # Shuffle deck
+        self.shuffle_cards()
+        self.shuffle_cards()
         self.shuffle_cards()
 
     def unshuffled_deck(self):
@@ -64,30 +88,46 @@ class Deck:
 
 
 class BlackJackHand():
+    name = ""
     hand = []
-    hand_total = 0
-    ranks = []
-    values = []
+    values = [999, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+             "Jack", "Queen", "King"]
 
-    def __init__(self, ranks, values):
-        self.ranks = ranks
-        self.values = values
+    def __init__(self, name):
+        self.name = name
+        # Needed - otherwise both dealer and hand point to same thing
+        self.hand = []
+
+    def __add__(self, card):
+        card_val = self.rank_to_val(card)
+        self.hand.append({'card': card, 'card_value': card_val})
 
     def add_card(self, card):
-        self.hand.append(card)
-        self.hand_total += self.rank_to_val(card)
+        card_val = self.rank_to_val(card)
+        self.hand.append({'card': card, 'card_value': card_val})
+
+    @property
+    def hand_value(self):
+        hand_value = 0
+        for card in self.hand:
+            hand_value += card['card_value']
+        return hand_value
+
+    @property
+    def hand_bust(self):
+        return self.hand_value > 21
 
     def clear_hand(self):
         self.hand = []
-        self.hand_total = 0
 
     def print_hand(self):
         if len(self.hand) == 0:
             print("Hand Empty")
         else:
-            print("Hand Value: ", self.hand_total)
+            print("Hand Value: ", self.hand_value)
             for card in self.hand:
-                print(card)
+                print(card['card'])
 
     def rank_to_val(self, card):
         inst_values = self.values
@@ -104,7 +144,6 @@ class BlackJackHand():
                 else:
                     print("Choice not valid - please try again")
                     ace_choice = 0
-
         return inst_values[self.ranks.index(card.rank)]
 
 
@@ -112,32 +151,70 @@ class BlackJackHand():
 class BlackJackDealer(Deck):
     hand = []
 
-    def __init__(self, ranks, values):
-        self.hand = BlackJackHand(ranks, values)
+    def __init__(self):
+        # Build and shuffle the deck
         super(BlackJackDealer, self).__init__()
+        # Initalize dealers hand and deal two cards
+        self.hand = BlackJackHand("Dealer")
+        # self.hand + self.draw_card()
+        # self.hand + self.draw_card()
 
-    def hit(self):
-        return self.draw_card()
+    def deal_dealer(self, hand):
+        if self.hand_value < 17:
+            card = self.dealer.draw_card()
+            print("Dealer has hit...")
+            self.hand + card
+        else:
+            print("Dealer has stayed...")
 
 
 class BlackJackGame():
-    values = [999, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-    ranks = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-             "Jack", "Queen", "King"]
-    dealer = BlackJackDealer(ranks, values)
-    hand1 = BlackJackHand(ranks, values)
+    dealer = BlackJackDealer()
+    player = BlackJackHand("Player")
+    print(id(player.hand))
+    print(id(dealer.hand.hand))
+    card = Card("King", "Red", "Clubs")
+    dealer.hand.add_card(card)
+    player.add_card(card)
+
+    print(player.name)
+    player.print_hand()
+    print(dealer.hand.name)
+    dealer.hand.print_hand()
+
+    def __init__(self):
+        print("Welcome to blackjack! Starting game now...")
+        self.start_game()
+
+    def start_game(self):
+        quit = False
+        while(not quit):
+            choice = None
+            while choice is None:
+                choice = input("Choose to: (Q)uit, (H)it, (S)tand - ")
+                if choice == "Q":
+                    quit = True
+                elif choice == "H":
+                    self.deal_player(self.player)
+                elif choice == "S":
+                    print("No Card Taken - Current Hand Value:",
+                          self.player.hand_value)
+                else:
+                    print("Choice not recognized - please try again")
+                    choice = None
+        print("Game Over")
+
+    def deal_player(self, hand):
+        card = self.dealer.draw_card()
+        # BlackJackGame.hand1.add_card(card)
+        hand + card
+        hand.print_hand()
+
+    def compare(self, dealer, player):
+        if dealer.hand_value > player.hand_value:
+            return "dealer"
+        else:
+            return "player"
 
 
 new_game = BlackJackGame()
-print("Welcome! Starting Game...")
-again = "y"
-while(again == "y"):
-    card = new_game.dealer.hit()
-    BlackJackGame.hand1.add_card(card)
-    BlackJackGame.hand1.print_hand()
-    if BlackJackGame.hand1.hand_total > 21:
-        print("Went over 21! Bust! - Clearing Hand")
-        BlackJackGame.hand1.clear_hand()
-        print("Next Round...")
-    again = input("Draw Again? y/n ")
-    print("-----------")
